ed9e49caff3a745e8a51d059f3d434565c603522
RoOt08 <reloadlife08@gmail.com>
2017-02-02 14:11:37 +0330

    Update SendPlug.lua

--- plugins/SendPlug.lua
+++ plugins/SendPlug.lua
@@ -3,11 +3,11 @@ local function run(msg, matches)
   if is_sudo(msg) then
 	if matches[1]:lower() == "send" and matches[2] and matches[3] then
 		local send_file = "./"..matches[2].."/"..matches[3]
-		tdcli.sendDocument(msg.chat_id_, msg.id_,0, 1, nil, send_file, '@BanG_TeaM :D')
+		tdcli.sendDocument(msg.chat_id_, msg.id_,0, 1, nil, send_file, '@RoOt08 :D')
 	end
 	if matches[1]:lower() == "send" and matches[2] then
 	    local plug = "./plugins/"..matches[2]..".lua"
-		tdcli.sendDocument(msg.chat_id_, msg.id_,0, 1, nil, plug, '@BanG_TeaM :D')
+		tdcli.sendDocument(msg.chat_id_, msg.id_,0, 1, nil, plug, '@RoOt08 :D')
     end
   end
 end
